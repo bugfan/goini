bfc638b098ff6585cc1765e4f20668c0722aa6b5
Fan <30284615+bugfan@users.noreply.github.com>
2018-12-24 11:58:37 +0800

    测试代理行不行

--- ini_test.go
+++ ini_test.go
@@ -26,7 +26,7 @@ func TestIni(t *testing.T) {
 	log.Println("开始读取环境变量---testing 2")
 	log.Println("读取 HOME", ENV.GetString("HOME", "nothing"))
 	log.Println("读取 GOPATH", ENV.GetString("GOPATH", "nothing"))
-	log.Println("读取 GOPATH", ENV.GetInt("NONE-ENV", 23)) //读取不到返回默认值
+	log.Println("读取 GOPATH", ENV.GetInt("NONE-ENV", 23)) 		//读取不到返回默认值,空串
 	// log.Println("读取 GOPATH", ENV.GetBool("GOPATH", "nothing"))
 	// log.Println("读取 GOPATH", ENV.GetFloat("GOPATH", "nothing"))
 	log.Println("开始测试环境变量相关的代码:")
@@ -41,5 +41,5 @@ func TestIni(t *testing.T) {
 	m2 := NewMyEnv(".env")
 	log.Println("NewMyEnv 去读配置文件的变量 ，带文件:", err, m2.Getenv("TEST"))
 
-	log.Println("NewMyEnv 去读配置文件的变量 GetAllenv，带文件:", err, m2.GetAllenv())
+	log.Println("NewMyEnv 去读配置文件的变量 GetAllenv,带文件:", err, m2.GetAllenv())
 }
